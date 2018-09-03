8377b7bf12937cfed9075b7e3155824f3d804dac
Manju Thomas <kudukacar@hotmail.com>
2018-09-03 17:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,5 +50,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (placeOrder()) {
+    return "Sorry, we don't have a credit card on file for you";}
+    else {
+  }
 }

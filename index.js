288f9acc0761f2113069245a0eeb379e7b1517ff
Manju Thomas <kudukacar@hotmail.com>
2018-09-03 17:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function total() {
 function removeFromCart(item) {
   for (i = 0; i < cart.length; i++) {
   if (item === cart[i].itemName) {
-    cart.splice[i, 1];
+    cart.splice(i, 1);
     return cart;
   }
   else {return"That item is not in your cart.";

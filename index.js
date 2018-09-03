63cdb8d624c2229b7a9c051ae27a29916888e2ac
Manju Thomas <kudukacar@hotmail.com>
2018-09-03 13:51:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var price = Math.floor(Math.random() * 100) + 1;
- var cart = {itemName: item, itemPrice: price};
+ var itemObject = {itemName: item, itemPrice: price};
  return "item has been added to your cart.";
 }
 
